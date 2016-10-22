6d33c3f1e1a99b19fb491a76fd101bc93edb53cb
Danil Shy <udavster@gmail.com>
2016-10-22 16:32:14 +0300

    Second task is ready

---
+++ CSharpHW/2/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private double result;
+        private void calculateButton_Click(object sender, RoutedEventArgs e) {
+            if (operation.SelectedValue == null) {
+                ResultsLabel.Content = "No operation selected";
+                return;
+            }
+            double leftOperand, rightOperand, result;
+            try {
+                try {
+                    leftOperand = double.Parse(leftArgument.Text);
+                    rightOperand = double.Parse(rightArgument.Text);
+                } catch (FormatException) {
+                    ResultsLabel.Content = "Format exception";
+                    return;
+                }
+                ResultsLabel.Content = leftOperand.ToString() + " " + rightOperand.ToString();
+                //string operationSymbol = (operationComboBox.SelectedValue ?? String.Empty).ToString();
+
+                string operationSymbol = operation.SelectedValue.ToString();
+                switch (operationSymbol) {
+                    case "+":
+                        result = leftOperand + rightOperand;
+                        break;
+                    case "-":
+                        result = leftOperand - rightOperand;
+                        break;
+                    case "*":
+                        result = leftOperand * rightOperand;
+                        break;
+                    case "/":
+                        result = leftOperand / rightOperand;
+                        break;
+                    case "^":
+                        result = Math.Pow(leftOperand, rightOperand);
+                        ResultsLabel.Content = "";
+                        MessageBox.Show("Power is made by Math.Pow, so the same rules applied to it as described at MSDN for Math.Pow");
+                        break;
+                    default:
+                        return;
+                }
+                this.result = result;
+                ResultsLabel.Content = String.Format("{0:0.00}", result);
+            } catch (OverflowException) {
+                ResultsLabel.Content = "Overflow exception";
+            } catch (Exception) {
+                ResultsLabel.Content = "Exception";
+                MessageBox.Show("Problem is unknown but handled. Try fixing input");
+            }
+            //MessageBox.Show(operationSymbol);
+        }
+
+        private void seeFullResult_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show(this.result.ToString());
+        }
+    }
+}
---
+++ CSharpHW/2/Numeric Data Types/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseTypes {
+    class Program {
+        static void Main(string[] args) {
+            FormatTypeInfo(typeof(sbyte), sbyte.MinValue, sbyte.MaxValue, default(sbyte));
+            FormatTypeInfo(typeof(byte), byte.MinValue, byte.MaxValue, default(byte));
+            FormatTypeInfo(typeof(short), short.MinValue, short.MaxValue, default(short));
+            FormatTypeInfo(typeof(ushort), ushort.MinValue, ushort.MaxValue, default(ushort));
+            FormatTypeInfo(typeof(int), int.MinValue, int.MaxValue, default(int));
+            FormatTypeInfo(typeof(uint), uint.MinValue, uint.MaxValue, default(uint));
+            FormatTypeInfo(typeof(long), long.MinValue, long.MaxValue, default(long));
+            FormatTypeInfo(typeof(ulong), ulong.MinValue, ulong.MaxValue, default(ulong));
+            FormatTypeInfo(typeof(float), float.MinValue, float.MaxValue, default(float));
+            FormatTypeInfo(typeof(decimal), decimal.MinValue, decimal.MaxValue, default(decimal));
+            FormatTypeInfo(typeof(double), double.MinValue, double.MaxValue, default(double));
+
+            Console.ReadLine();
+        }
+
+        static void FormatTypeInfo(Type type, object minValue, object maxValue, object Default) {
+            Console.Write("{0} has MinValue of ", type.FullName);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(minValue.ToString());
+            Console.ResetColor();
+            Console.Write(", MaxValue of ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write( maxValue.ToString());
+            Console.ResetColor();
+            Console.Write(", default value of ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(Default.ToString());
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+
+    }
+
+
+}
